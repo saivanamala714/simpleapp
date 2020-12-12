dbf13feb35c1c3cbc4aee2e29e225782815cea72
sairamprasadvanamala <saivanamala714@gmail.com>
2020-12-11 20:33:18 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore((state={}, action)=>{
+    switch (action.type) {
+        case 'ADD':
+            return action.name;
+        default:
+            return state;
+    }
+}, {name: 'sai'}, composeWithDevTools(
+    applyMiddleware(),
+    // other store enhancers if any
+));
+
+function App() {
+  return (
+    <div className="App">
+      <h1>{JSON.stringify(store.getState())}</h1>
+    </div>
+  );
+}
+
+export default App;
